2e1552527a227e839eff407a16d395d604366060
Álvaro Castellano Vela <alvaro.castellano.vela@gmail.com>
2021-04-20 23:10:46 +0200

    Finish backup script

--- Backups/MySQL/mydumper_backup.sh
+++ Backups/MySQL/mydumper_backup.sh
@@ -44,12 +44,45 @@ BACKUPP_FOLDER="/mnt/mysql/backup"
         fi
 }
 
-function create_backup_folder (){
-        [[ -v BACKUPP_FOLDER ]]; backup_folder_is_defined=$?
-        if (( $backup_folder_is_defined != 0 )); then
-BACKUPP_FOLDER="/mnt/mysql/backup"
+function set_retain_days (){
+        [[ -v BACKUP_RETAIN_DAYS ]]; backup_retain_days_is_defined=$?
+        if (( $backup_retain_days_is_defined != 0 )); then
+BACKUP_RETAIN_DAYS="8"
         fi
 }
+
+
+function create_backup_folder (){
+        mkdir -p ${BACKUPP_FOLDER}
+        backup_folder_has_been_created=$?
+        if (( $backup_folder_has_been_created != 0 )); then
+exit 1
+        fi
+}
+
+function perform_backup (){
+        backup_folder_name=$(date +'%Y_%m_%d')
+        test -d "${BACKUPP_FOLDER}/${backup_folder_name}"; backup_folder_already_exists=$? 
+        if (( $backup_folder_already_exists == 0 )); then
+                report_error "${BACKUPP_FOLDER}/${backup_folder_name} already exists."
+                exit 1
+        else
+        mydumper --outputdir "${BACKUPP_FOLDER}/${backup_folder_name}" --lock-all-tables --compress
+        backup_has_been_succeeded=$?
+        if (( $backup_has_been_succeeded != 0 )); then
+exit 1
+        fi
+        fi
+}
+
+function remove_old_backups (){
+        find ${BACKUPP_FOLDER} -mindepth 1 -maxdepth 1  -daystart -mtime +${BACKUP_RETAIN_DAYS} -delete
+        delete_error=$? 
+        if (( $delete_error != 0 )); then
+exit 1
+        fi
+}
+
 
 ##########
 ## Main ##
@@ -57,6 +90,9 @@ BACKUPP_FOLDER="/mnt/mysql/backup"
 
  check_mydumper
  check_backup_folder
+ set_retain_days
 
  create_backup_folder
- echo ${BACKUPP_FOLDER}
+ perform_backup
+
+ remove_old_backups
